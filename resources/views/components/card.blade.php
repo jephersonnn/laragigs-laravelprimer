09c7b3b80b4f5e0b2470207dcab89572ecc4ffee
Jeph <ericjeffrey.sch@gmail.com>
2023-03-03 11:07:01 +0800

    implement component for single listing

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,6 @@
+<div class="bg-gray-50 border border-gray-200 rounded p-6">
+    {{$slot}} {{--this component may be used to wrap blocks of code into a div--}}
+    {{--
+        to render the codes into this component as a prop, wrap those codes with <x-card></x-card>
+    --}}
+</div>
