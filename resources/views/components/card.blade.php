869f103a8ac112f6105c21146f7e6a6e98e505c8
Jeph <ericjeffrey.sch@gmail.com>
2023-03-03 11:12:15 +0800

    implement component card for single listing

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,5 @@
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<div {{$attributes->merge(['class'=>"bg-gray-50 border border-gray-200 rounded p-6"])}}> 
+    {{-- $attributes->merge allows the class of this div to be overidden in case if the component is called to render with additional attribute--}}
     {{$slot}} {{--this component may be used to wrap blocks of code into a div--}}
     {{--
         to render the codes into this component as a prop, wrap those codes with <x-card></x-card>
