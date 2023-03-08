df0320591b1ca43d2b117aec1c689121501e40aa
Jeph <ericjeffrey.sch@gmail.com>
2023-03-08 11:19:29 +0800

    add auth links for logged in user

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,6 +30,17 @@
     <nav class="flex justify-between items-center mb-4">
         <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
+            {{--  start of auth directive --}}
+            @auth
+            <li>
+               <span class="font-bold uppercase">Welcome {{auth()->user()->name}} !!</span>
+            </li>
+            <li>
+                <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i>
+                    Manage Listings</a>
+            </li>
+            {{-- if logged out --}}
+            @else
             <li>
                 <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
             </li>
@@ -37,6 +48,8 @@
                 <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
                     Login</a>
             </li>
+            @endauth
+            {{-- end of auth directive --}}
         </ul>
     </nav>
     <main class="pb-40">
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -13,7 +13,7 @@
                 <label for="name" class="inline-block text-lg mb-2">
                     Name
                 </label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name" value={{old('name')}} />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name" value="{{old('name')}}" />
                 @error('name')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
@@ -21,7 +21,7 @@
 
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Email</label>
-                <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" value={{old('email')}}/>
+                <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}"/>
                 @error('email')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
@@ -31,7 +31,7 @@
                 <label for="password" class="inline-block text-lg mb-2">
                     Password
                 </label>
-                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password" value={{old('password')}}/>
+                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password" value="{{old('password')}}"/>
                 @error('password')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
